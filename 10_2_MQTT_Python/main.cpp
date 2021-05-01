d39bb031abc918a19fdeecdaa497183c411fa9c1
evelyn655 <evelyn108061124@gapp.nthu.edu.tw>
2021-05-01 03:14:24 -0700

    add

--- 10_2_MQTT_Python/main.cpp
+++ 10_2_MQTT_Python/main.cpp
@@ -2,29 +2,57 @@
 #include "MQTTNetwork.h"
 #include "MQTTmbed.h"
 #include "MQTTClient.h"
+#include "stm32l475e_iot01_accelero.h"
 
 // GLOBAL VARIABLES
 WiFiInterface *wifi;
-InterruptIn btn2(USER_BUTTON);
+//InterruptIn btn2(USER_BUTTON);
 //InterruptIn btn3(SW3);
+InterruptIn btnRecord(USER_BUTTON);
 volatile int message_num = 0;
 volatile int arrivedcount = 0;
 volatile bool closed = false;
 
-const char* topic = "Mbed";
 
 Thread mqtt_thread(osPriorityHigh);
+Thread t;
 EventQueue mqtt_queue;
+EventQueue queue(32 * EVENTS_EVENT_SIZE);
 
+int16_t pDataXYZ[3] = {0};
+int idR[32] = {0};
+int indexR = 0;
+
+const char* topic = "Mbed";
+
+void record(void) {
+   BSP_ACCELERO_AccGetXYZ(pDataXYZ);
+   printf("%d, %d, %d\n", pDataXYZ[0], pDataXYZ[1], pDataXYZ[2]);
+}
+
+void startRecord(void) {
+   printf("---start---\n");
+   idR[indexR++] = queue.call_every(1ms, record);
+   indexR = indexR % 32;
+}
+
+void stopRecord(void) {
+   printf("---stop---\n");
+   for (auto &i : idR)
+      queue.cancel(i);
+}
+ 
 void messageArrived(MQTT::MessageData& md) {
     MQTT::Message &message = md.message;
     char msg[300];
     sprintf(msg, "Message arrived: QoS%d, retained %d, dup %d, packetID %d\r\n", message.qos, message.retained, message.dup, message.id);
-    printf(msg);
+    //printf(msg);
+    // 以上印標頭（訊息類型、DUP(是否為副本)、QoS(傳輸品質)、Retain(是否保留)）
     ThisThread::sleep_for(1000ms);
     char payload[300];
     sprintf(payload, "Payload %.*s\r\n", message.payloadlen, (char*)message.payload);
-    printf(payload);
+    //printf(payload);
+    // 以上印 arrived 的東西
     ++arrivedcount;
 }
 
@@ -32,7 +60,8 @@ void publish_message(MQTT::Client<MQTTNetwork, Countdown>* client) {
     message_num++;
     MQTT::Message message;
     char buff[100];
-    sprintf(buff, "QoS0 Hello, Python! #%d", message_num);
+    //sprintf(buff, "QoS0 Hello, Python! #%d", message_num);
+    sprintf(buff, "%d, %d, %d\n", pDataXYZ[0], pDataXYZ[1], pDataXYZ[2]);
     message.qos = MQTT::QOS0;
     message.retained = false;
     message.dup = false;
@@ -97,14 +126,28 @@ int main() {
             printf("Fail to subscribe\r\n");
     }
 
+    BSP_ACCELERO_Init();
+    t.start(callback(&queue, &EventQueue::dispatch_forever));
+    btnRecord.fall(queue.event(startRecord));
+    btnRecord.rise(queue.event(stopRecord));
+
     mqtt_thread.start(callback(&mqtt_queue, &EventQueue::dispatch_forever));
-    btn2.rise(mqtt_queue.event(&publish_message, &client));
+    mqtt_queue.call_every(500, &publish_message, &client);                // call to publish message every 0.5 second
+    //btn2.rise(mqtt_queue.event(&publish_message, &client));
     //btn3.rise(&close_mqtt);
 
     int num = 0;
     while (num != 5) {
-            client.yield(100);
-            ++num;
+        client.yield(100);
+
+        // int yield	(unsigned long 	timeout_ms = 1000L )	
+        // A call to this API must be made within the keepAlive interval to keep the MQTT connection alive yield can be called if no other MQTT operation is needed.
+
+        // This will also allow messages to be received.
+
+        // Parameters: timeout_ms	the time to wait, in milliseconds
+        // Returns: success code - on failure, this means the client has disconnected
+        ++num;
     }
 
     while (1) {
